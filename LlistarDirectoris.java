14402eca4cf237b2469c0213ae535e79f614eb9d
jolare1 <32924394+jolare1@users.noreply.github.com>
2017-11-13 23:48:58 +0100

    Update LlistarDirectoris.java

--- LlistarDirectoris.java
+++ LlistarDirectoris.java
@@ -1,8 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    CORRESPON ALS EXERCICIS 1, 2, 3 i 5 el 4 el tenia que fer recursivament i 
+    encara no s'havia com fer-ho per tant he tingut que fer un altre ex
+*/
 
 
 import java.io.File;
@@ -15,30 +14,30 @@ public class LlistarDirectoris
     // Lista el contenido de un fichero e indica cual el fichero y cual es directorio
     public static void InformacionDeFicherosYDirectorios()
     {
-        File directorio = new File("C:\\Users\\MiSeñor\\Documents");
+        File directorio = new File("C:\\Users\\MiSeÃ±or\\Documents");
         if(directorio.isDirectory())
         {
             String[] listado = directorio.list();
             System.out.println("DIRECTORIOS:\t");
             for(int i = 0; i < listado.length; i++)
             {
-                File examinar = new File("C:\\Users\\MiSeñor\\Documents\\" + listado[i]);
+                File examinar = new File("C:\\Users\\MiSeÃ±or\\Documents\\" + listado[i]);
                 if(examinar.isDirectory())
                 {
                     System.out.println("DIRECTORIO:\t" + listado[i]);
-                    File directorioNivel1 = new File("C:\\Users\\MiSeñor\\Documents\\" + listado[i]);
+                    File directorioNivel1 = new File("C:\\Users\\MiSeÃ±or\\Documents\\" + listado[i]);
                     String[] listadoNivel1 = directorioNivel1.list();
                 	for(int j = 0; j < listadoNivel1.length; j++)
                 	{
-                		File examinarNivel1 = new File("C:\\Users\\MiSeñor\\Documents\\" + listado[i] + "\\" + listadoNivel1[j]);
+                		File examinarNivel1 = new File("C:\\Users\\MiSeÃ±or\\Documents\\" + listado[i] + "\\" + listadoNivel1[j]);
                 		if(examinarNivel1.isDirectory()) 
                 		{
                 			System.out.println("\tDIRECTORIO:\t" + listadoNivel1[j]);
-                			File directorioNivel2 = new File("C:\\Users\\MiSeñor\\Documents\\" + listado[i] + "\\" + listadoNivel1[j]);
+                			File directorioNivel2 = new File("C:\\Users\\MiSeÃ±or\\Documents\\" + listado[i] + "\\" + listadoNivel1[j]);
                 			String[] listadoNivel2 = directorioNivel2.list();
                 			for(int k = 0; k < listadoNivel2.length; k++)
                 			{
-                				File examinarNivel2 = new File("C:\\Users\\MiSeñor\\Documents\\" + listado[i] + "\\" + listadoNivel1[j] + "\\" + listadoNivel2[k]);
+                				File examinarNivel2 = new File("C:\\Users\\MiSeÃ±or\\Documents\\" + listado[i] + "\\" + listadoNivel1[j] + "\\" + listadoNivel2[k]);
                 				if(examinarNivel2.isDirectory())
                 					System.out.println("\t\tDIRECTORIO:\t" + listadoNivel2[k]);
                 				else 
@@ -53,7 +52,7 @@ public class LlistarDirectoris
             System.out.println("FICHEROS:\t");
             for(int i = 0; i < listado.length; i++)
             {
-                File examinar = new File("C:\\Users\\MiSeñor\\Documents\\" + listado[i]);
+                File examinar = new File("C:\\Users\\MiSeÃ±or\\Documents\\" + listado[i]);
             	 if(!examinar.isDirectory())
                      System.out.println("FICHERO:\t" + listado[i]);
             }
