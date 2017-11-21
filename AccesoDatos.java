a161be3d3d489dae659fcbbd5ed2f9581d4f8b43
jolare1 <32924394+jolare1@users.noreply.github.com>
2017-11-21 18:56:42 +0100

    21-11-17 Practica 2

--- AccesoDatos.java
+++ AccesoDatos.java
@@ -1,19 +1,25 @@
 import java.sql.*;
 
+import com.sun.swing.internal.plaf.basic.resources.basic_fr;
+import com.sun.xml.internal.bind.v2.runtime.reflect.opt.Const;
+
 public class AccesoDatos {
     public static void main(String[] args) {
     	AccesoDatos bd = new AccesoDatos();
     	bd.abrirConexion();
     	bd.obtenerDatosTabla();
-    	bd.mostrarDatos();
-    	bd.modificarDatos();
-    	bd.insertarDatos();
-    	bd.borrar();
+    	//bd.mostrarDatos();
+    	//bd.modificarDatos();
+    	//bd.insertarDatos();
+    	//bd.borrar();
+    	bd.insertarProjectos(11);//li pasem el codi del client del qual farem el projecte
+    	bd.borrarClient(0);//li pasarem el codi del client , la funció ens borrara el client i els seus projectes
     	bd.cerrarConexion();  	
     }
 	Connection con;
 	Statement st;
 	ResultSet rs;
+	int clienteExiste = 0;
 
 	public void abrirConexion() {
 		try {
@@ -70,7 +76,6 @@ public class AccesoDatos {
 	{ 
 		try {
 			st = con.createStatement();
-			//quito el num_project porque si no no va
 			st.executeUpdate("INSERT INTO empleats (codi_empl, nom_empl, cognom_empl, sou, nom_dpt, ciutat_dpt) values (9, 'JOAN','LAJARIN', 666, 'DIS', 'BARCELONA')");
 			System.out.println("Elemento insertado");
 		}
@@ -97,6 +102,59 @@ public class AccesoDatos {
 			System.out.println("Conexión cerrada");
 		} catch (SQLException e) {
 			System.out.println("Error al cerrar conexión");
+		}
+	}
+	
+	public void validarCodiClient(int codi_client) {
+		try {
+			 st =con.createStatement();
+			 rs = st.executeQuery("SELECT * FROM clients WHERE codi_cli=" + codi_client);
+			 String codi_Cli = "";
+			 while (rs.next()) {
+			 codi_Cli = rs.getString("codi_cli");
+			 System.out.println(codi_Cli);
+			 }
+			 
+			 if (codi_Cli == "") {
+				 clienteExiste = 0;
+			 }	
+			 else {
+				 clienteExiste = 1;
+			 }
+		}
+		catch(SQLException e) {
+			
+		}
+	}
+	public void insertarProjectos(int codi_client)
+	{ 
+		try {
+			validarCodiClient(codi_client);
+			if(clienteExiste == 1) {
+				st = con.createStatement();
+				st.executeUpdate("INSERT INTO projectes (codi_proj, nom_proj, preu, data_inici, data_prev_fi, data_fi, codi_client ) values (6, 'JEfJE', 666, '1997-11-11', '1997-11-11', '1997-11-11', " + codi_client + ")");
+				System.out.println("Elemento insertado");
+			}
+			else if (clienteExiste == 0) {
+				System.out.println("No existe el cliente");
+			}
+			else {
+				System.out.println("Error");
+			}
+		}
+		catch(SQLException e)
+		{
+			System.out.println("Error al insertar ");
+		}
+	}
+	public void borrarClient(int codi_client) {
+		try {
+			st = con.createStatement();
+			st.executeUpdate("DELETE FROM projectes where codi_client=" + codi_client);
+			st.executeUpdate("DELETE FROM clients where codi_cli=" + codi_client);
+			System.out.println("Elemento Borrado");
+		} catch (SQLException e) {
+			System.out.println("Error al Borrar");
 		}
 	}
 }
