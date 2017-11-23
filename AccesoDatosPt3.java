d4183dfdbb4611e80a51f3b62f2fe8b671aae804
jolare1 <32924394+jolare1@users.noreply.github.com>
2017-11-23 11:29:13 +0100

    Update AccesoDatosPt3.java

--- AccesoDatosPt3.java
+++ AccesoDatosPt3.java
@@ -10,8 +10,8 @@ public class AccesoDatosPt3 {
 	ResultSet rs;
     
   public static void main(String[] args) {
-    	AccesoDatosPt3 bd = new AccesoDatosPt3();
-    	bd.abrirConexion();
+      AccesoDatosPt3 bd = new AccesoDatosPt3();
+      bd.abrirConexion();
       bd.getDatabaseMetaData();
       bd.getDatabaseTableData();
       bd.cerrarConexion();
@@ -24,19 +24,19 @@ public class AccesoDatosPt3 {
 			String url = "jdbc:mysql://localhost/basedas";
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
 			con = DriverManager.getConnection(url, userName, password);
-			System.out.println("ConexiÛn a la BD");
+			System.out.println("Conexi√≥n a la BD");
 		} catch (Exception e) {
-			System.out.println("Error en conexiÛn ");
+			System.out.println("Error en conexi√≥n ");
 		}
 	}
 	
-	// Para cerrar la conexiÛn una vez terminadas las consultas
+	// Para cerrar la conexi√≥n una vez terminadas las consultas
 	public void cerrarConexion() {
 		try {
 			con.close();
-			System.out.println("ConexiÛn cerrada");
+			System.out.println("Conexi√≥n cerrada");
 		} catch (SQLException e) {
-			System.out.println("Error al cerrar conexiÛn");
+			System.out.println("Error al cerrar conexi√≥n");
 		}
 	}
   public void getDatabaseMetaData() {
@@ -45,18 +45,18 @@ public class AccesoDatosPt3 {
       String driverName = dbmd.getDriverName();
       String url = dbmd.getURL();
       String userName = dbmd.getUserName();
-      System.out.println("META INFORMACI” DE LA BASE DE DADES: NOM: " + productName + "DRIVER: " + driverName + "URL: " + url + "USUARI: " + userName);
+      System.out.println("META INFORMACI√ì DE LA BASE DE DADES: NOM: " + productName + "DRIVER: " + driverName + "URL: " + url + "USUARI: " + userName);
   }
      
   public void getDatabaseTableData() {
       rs = dbmd.getTables(null, "basedas", null, null);
-      String catalegName = rs.getString(cat‡leg);
+      String catalegName = rs.getString(cat√†leg);
       String esquemaName = rs.getString(esquema);
       String taulaName = rs.getString(taula);
       String tipus = rs.getString(tipus);
       System.out.println("TABLE - cataleg: " + catalegName + "esquema: " + esquemaName +  "taula: " + taulaName + tipus);
       /*while (rs.next()) {
-         String catalegName = rs.getString(cat‡leg);
+         String catalegName = rs.getString(cat√†leg);
          String esquemaName = rs.getString(esquema);
          String taulaName = rs.getString(taula);
          String tipus = rs.getString(tipus);
